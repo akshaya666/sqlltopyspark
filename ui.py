6301f9bde91ce97e9e5f16d0d494c26822592cf1
akshaya666 <60505338+akshaya666@users.noreply.github.com>
2024-08-06 15:01:06 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -4,16 +4,12 @@ from datetime import timedelta
 import boto3
 from search_handler import SearchHandler
 from text_processor import TextProcessor
-import  search_handler
 import json
+
 app = Flask(__name__)
 app.secret_key = 'sdf34987tymns037ut3n0tu30jrgj3klfgu430g98q90gro'
 app.config['UPLOAD_FOLDER'] = 'uploads/'
-# app.config['SESSION_COOKIE_SECURE'] = False
-# app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=5)
 
-
-# Dummy list of documents
 DOCUMENTS = ['Sample', 'Document 2', 'Document 3']
 s3_client = boto3.client('s3')
 bucket_name = "app-id-25591-dep-id-111615-uu-id-x80cfnuken7o"
@@ -28,8 +24,6 @@ def home():
     session['selected_document'] = DOCUMENTS[0]  # Set default selected document
     return render_template('index.html', section='home', documents=DOCUMENTS)
 
-
-
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_files():
     if request.method == 'POST':
@@ -41,18 +35,7 @@ def upload_files():
         for file in files:
             if file and file.filename:
                 file_path = os.path.join(uploads_folder_path, file.filename)
-                s3_client.upload_fileobj(file,bucket_name,file_path) ## uploads pdf data to raw_data folder
-
-                ############################# data extraction and store it in json in s3 in processed_data folder###########################
-
-                ############################################### chunking ##########################################
-                # processed_file_path = os.path.join(processed_folder_path, file.filename)
-                # text_processor = TextProcessor(bucket_name,s3_client,processed_file_path)
-                # text_processor.process_text()
-                # chunk_file_path = os.path.join(chunks_folder_path,file.filename)
-                # text_processor.save_chunks_to_s3(chunk_file_path)
-                ############################################### chunking ##########################################
-
+                s3_client.upload_fileobj(file, bucket_name, file_path)
                 uploaded_files.append(file.filename)
 
         return render_template('index.html', section='upload', uploaded_files=uploaded_files, documents=DOCUMENTS)
@@ -61,24 +44,19 @@ def upload_files():
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
     if request.method == 'POST':
+        message = request.form.get('message', '').strip()
+        if message:
+            session['chat_history'].append({'role': 'user', 'message': message})
+            chunk_file = chunks_folder_path + session.get('selected_document') + '.json'
+            read_chunk_file = s3_client.get_object(Bucket=bucket_name, Key=chunk_file)
+            text_chunks = json.loads(read_chunk_file['Body'].read().decode('utf-8'))
+            search_handler_instance = SearchHandler(message, text_chunks)
+            search_answer = search_handler_instance.get_precise_answer()
+            bot_message = f"{search_answer}"
+            session['chat_history'].append({'role': 'bot', 'message': bot_message})
+            return jsonify({'message': bot_message})
 
-        if 'message' in request.form:
-            message = request.form.get('message', '').strip()
-            if message:
-                session['chat_history'].append({'role': 'user', 'message': message})
-                # Simulate a bot response
-                chunk_file = chunks_folder_path + session.get('selected_document') + '.json'
-                print(chunk_file)
-                read_chunk_file = s3_client.get_object(Bucket=bucket_name,Key=chunk_file)
-                text_chunks = json.loads(read_chunk_file['Body'].read().decode('utf-8'))
-                print(text_chunks[0])
-                search_handler_instance = SearchHandler(message, text_chunks)
-                search_answer = search_handler_instance.get_precise_answer()
-                bot_message = f"{search_answer}"
-                session['chat_history'].append({'role': 'bot', 'message': bot_message})
-                session['dummy']='2'
-
-    return render_template('index.html', section='chat', chat_history=session['chat_history'], documents=DOCUMENTS ,selected_document=session.get('selected_document'))
+    return render_template('index.html', section='chat', chat_history=session['chat_history'], documents=DOCUMENTS, selected_document=session.get('selected_document'))
 
 @app.route('/clear_chat')
 def clear_chat():
